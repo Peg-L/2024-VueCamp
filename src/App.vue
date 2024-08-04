75f152bc68afa85f7699241d35af624eb10a2d34
Peggy Lin <stu90233@gmail.com>
2024-08-04 22:47:25 +0800

    feat: 🎸 first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref } from 'vue'
+const num = ref(0)
+</script>
+
+<template>
+  <div class="d-flex">
+    <button type="button" @click="num--">-</button>
+    <span class="px-10">
+      {{ num }}
+    </span>
+    <button type="button" @click="num++">+</button>
+  </div>
+</template>
+
+<style scoped>
+.d-flex {
+  display: flex;
+}
+
+.px-10 {
+  padding: 0 40px;
+}
+</style>
